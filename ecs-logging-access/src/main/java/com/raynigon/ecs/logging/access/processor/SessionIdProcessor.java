5fe481fc512c3682ad0b6b152c30b6297a44dd5f
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-07-12 13:55:02 +0200

    Update Test Cases

--- ecs-logging-access/src/main/java/com/raynigon/ecs/logging/access/processor/SessionIdProcessor.java
+++ ecs-logging-access/src/main/java/com/raynigon/ecs/logging/access/processor/SessionIdProcessor.java
@@ -8,8 +8,11 @@ import static com.raynigon.ecs.logging.LoggingConstants.SESSION_ID_HEADER;
 public class SessionIdProcessor implements AccessEventProcessor {
     @Override
     public EcsAccessLogEvent process(EcsAccessLogEvent result, IAccessEvent event) {
+        String sessionId = event.getRequestHeader(SESSION_ID_HEADER);
+        if (IAccessEvent.NA.equals(sessionId))
+            return result;
         return result.toBuilder()
-                .sessionId(event.getResponseHeader(SESSION_ID_HEADER))
+                .sessionId(sessionId)
                 .build();
     }
 }
