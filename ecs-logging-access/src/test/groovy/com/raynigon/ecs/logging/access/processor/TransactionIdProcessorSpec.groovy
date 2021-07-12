afda4f47b8ec24ff6f8460d587f7622f44849754
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-07-12 14:11:33 +0200

    Update Test Cases

---
+++ ecs-logging-access/src/test/groovy/com/raynigon/ecs/logging/access/processor/TransactionIdProcessorSpec.groovy
@@ -0,0 +1,27 @@
+package com.raynigon.ecs.logging.access.processor
+
+import ch.qos.logback.access.spi.IAccessEvent
+import com.raynigon.ecs.logging.access.event.EcsAccessLogEvent
+import spock.lang.Specification
+import spock.lang.Subject
+
+class TransactionIdProcessorSpec extends Specification {
+
+    @Subject
+    TransactionIdProcessor processor = new TransactionIdProcessor()
+
+    def "transaction id is given"() {
+        given:
+        IAccessEvent event = Mock()
+        event.getRequestHeader(_ as String) >> "test 123"
+
+        and:
+        EcsAccessLogEvent result = EcsAccessLogEvent.builder().build()
+
+        when:
+        result = processor.process(result, event)
+
+        then:
+        result.sessionId == "test 123"
+    }
+}
