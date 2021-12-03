b8b92ba619eaca976ae4d4483f62ca594179ee73
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-12-03 13:36:46 +0100

    Implement SessionId and TransactionId Handling
    SessionId and TransactionId should be passed with all client calls

--- ecs-logging-access/src/main/java/com/raynigon/ecs/logging/access/client/AccessLogExchangeFilterFunction.java
+++ ecs-logging-access/src/main/java/com/raynigon/ecs/logging/access/client/AccessLogExchangeFilterFunction.java
@@ -3,11 +3,14 @@ package com.raynigon.ecs.logging.access.client;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.MDC;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.*;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -15,22 +18,13 @@ import java.util.UUID;
 
 import static com.raynigon.ecs.logging.LoggingConstants.*;
 
-@Configuration
-@RequiredArgsConstructor
-@ConditionalOnClass(value = {RestTemplate.class})
-public class RestTemplateConfiguration {
 
-    private final List<RestTemplate> templates;
+class AccessLogExchangeFilterFunction implements ExchangeFilterFunction {
 
-    @PostConstruct
-    public void configure() {
-        templates.forEach(this::configure);
-    }
-
-    public void configure(RestTemplate restTemplate) {
-        restTemplate.getInterceptors().add((HttpRequest request, byte[] body, ClientHttpRequestExecution execution) -> {
-            new AccessLogHeaderFilter().accept(request.getHeaders());
-            return execution.execute(request, body);
-        });
+    @Override
+    public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
+        new AccessLogHeaderFilter().accept(request.headers());
+        return next.exchange(request);
     }
 }
+
