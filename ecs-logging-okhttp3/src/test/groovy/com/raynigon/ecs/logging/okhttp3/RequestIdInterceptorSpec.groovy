43d6385c1c0bedb0d7206acb676ada7b4320ef17
simon <10846939+raynigon@users.noreply.github.com>
2024-01-29 12:05:14 +0100

    Implement Interceptor for OkHttp3

---
+++ ecs-logging-okhttp3/src/test/groovy/com/raynigon/ecs/logging/okhttp3/RequestIdInterceptorSpec.groovy
@@ -0,0 +1,12 @@
+package com.raynigon.ecs.logging.okhttp3;
+
+import spock.lang.Specification
+
+class RequestIdInterceptorSpec extends Specification {
+
+    def "test"() {
+        expect:
+        false
+    }
+
+}
